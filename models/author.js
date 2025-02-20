47c0f11ed99cf8551d3f7270b104bbcda56f4da5
Vincent Allport <vincentallport@gmail.com>
2025-02-19 19:47:06 -0500

    created baby's first model

---
+++ models/author.js
@@ -0,0 +1,7 @@
+const authors = [
+    {firstName: 'Suzanne', lastName: 'Clarke'}, 
+    {firstName: 'Robin', lastName: 'Hobb'},
+    {firstName: 'J.R.R.', lastName: 'Tolkien'}
+]
+
+exports.all = authors
--- routes/authors.js
+++ routes/authors.js
@@ -1,12 +1,10 @@
 const express = require('express')
 const router = express.Router()
 
+const Author = require('../models/author')
+
 router.get('/', function(req, res, next) {
-    const authors = [
-        {firstName: 'Suzanne', lastName: 'Clarke'}, 
-        {firstName: 'Robin', lastName: 'Hobb'},
-        {firstName: 'J.R.R.', lastName: 'Tolkien'}
-    ]
+    const authors = Author.all
     res.render('authors/index', {title: 'BookBok / Authors', authors: authors})
 })
 
