06a0cf8b5f84e83ce5b9cd8de0b9c3e57005fc16
Vincent Allport <vincentallport@gmail.com>
2025-02-19 19:50:21 -0500

    updated books file

--- routes/books.js
+++ routes/books.js
@@ -2,7 +2,7 @@ const express = require('express')
 const router = express.Router()
 
 router.get('/', function(req, res, next) {
-    const books = ['Piranesi', 'Ship of Magic', 'The Lord of the Rings']
+    const books = ['Piranesi', 'Ship of Magic', 'The Lord of the Rings', 'Wicked']
     res.render('books/index', {title: 'BookBok / Books', books: books})
 })
 
