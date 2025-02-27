1c60ffd57fc61ceab6f09cfdc7957b14c569a6a5
Vincent Allport <vincentallport@gmail.com>
2025-02-26 20:41:43 -0500

    added handlebars helpers

--- bookbok.js
+++ bookbok.js
@@ -1,5 +1,23 @@
 const express = require('express'); // import express
-const handlebars = require('express-handlebars').create() // create handlebars object
+const handlebars = require('express-handlebars').create({ // create handlebars object
+    helpers: {
+      eq: (v1, v2) => v1 == v2,
+      ne: (v1, v2) => v1 != v2,
+      lt: (v1, v2) => v1 < v2,
+      gt: (v1, v2) => v1 > v2,
+      lte: (v1, v2) => v1 <= v2,
+      gte: (v1, v2) => v1 >= v2,
+      and() {
+          return Array.prototype.every.call(arguments, Boolean);
+      },
+      or() {
+          return Array.prototype.slice.call(arguments, 0, -1).some(Boolean);
+      },
+      someId: (arr, id) => arr && arr.some(obj => obj.id == id),
+      in: (arr, obj) => arr && arr.some(val => val == obj),
+      dateStr: (v) => v && v.toLocaleDateString("en-US")
+    }
+  });
 const bodyParser = require('body-parser')
 
 // import routers from routes folder
