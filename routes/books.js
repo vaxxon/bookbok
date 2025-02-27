49c34defad361d753b428e06bd39eb8d839ee1e4
Vincent Allport <vincentallport@gmail.com>
2025-02-26 20:27:24 -0500

    added book detail page

--- routes/books.js
+++ routes/books.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const router = express.Router()
-
 const Book = require('../models/book')
+const Author = require('../models/author')
 
 router.get('/', function(req, res, next) {
     const books = Book.all
@@ -10,7 +10,7 @@ router.get('/', function(req, res, next) {
 
 // form submission route
 router.get('/form', async(req, res, next) => {
-    res.render('books/form', {title : 'BookBok / Books'})
+    res.render('books/form', {title : 'BookBok / Books', authors: Author.all})
 })
 
 // book creation route
@@ -24,7 +24,19 @@ router.post('/upsert', async(req, res, next) => {
 router.get('/edit', async(req, res, next) => {
     let bookIndex = req.query.id;
     let book = Book.get(bookIndex);
-    res.render('books/form', { title: "BookBok / Edit Book", book: book, bookIndex: bookIndex })
+    res.render('books/form', { title: "BookBok / Edit Book", book: book, bookIndex: bookIndex, authors: Author.all })
+})
+
+// book detail route
+router.get('/show/:id', async (req, res, next) => {
+    let templateVars = {
+        title: "BookBok / Books",
+        book: Book.get(req.params.id)
+    }
+    if("authorId" in templateVars.book) {
+        templateVars['author'] = Author.get(templateVars.book.authorId)
+    }
+    res.render('books/show', templateVars)
 })
 
 module.exports = router
