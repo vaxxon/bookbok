23dfc27971ab60aaa277455756be1bef3e54d3fd
Vincent Allport <vincentallport@gmail.com>
2025-04-01 19:30:13 -0400

    initiate comment model

---
+++ models/comment.js
@@ -0,0 +1,39 @@
+const comments = [
+    {id: '0', bookId: '0', userEmail: 'vallport@pratt.edu', text: 'Wow what a book. Incredible. Two stars.'},
+    {id: '1', bookId: '0', userEmail: 'vallport@pratt.edu', text: 'Actually nevermind this book sucks. Four stars.' },
+    {id: '2', bookId: '0', userEmail: 'vallport@pratt.edu', text: 'I recommended this book to my dad and he tried to fight me. Five stars.'
+    }
+]
+
+function getNextId() {
+    return Math.max(...comments.map(c => c.id)) + 1
+}
+
+exports.add = (comment) => {
+    comment.id = getNextId()
+    comments.push(comment)
+}
+
+exports.update = (comment) => {
+    comments[comment.id] = comment
+}
+
+exports.upsert = (comment) => {
+    if (comment.id) {
+        exports.update(comment)
+    } else {
+        exports.add(comment)
+    }
+}
+
+exports.get = (id) => {
+    return comments.find((comment) => {
+        return comment.id == id
+    })
+}
+
+exports.allForBook = (bookId) => {
+    return comments.filter((comment) => {
+        return comment.bookId == bookId
+    })
+}
