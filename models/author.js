8f99c6e0956fe9bbcb216ad4f44007d1b487daa3
Vincent Allport <vincentallport@gmail.com>
2025-04-16 19:38:58 -0400

    adjusted to link to authors db

--- models/author.js
+++ models/author.js
@@ -1,5 +1,34 @@
 const db = require('../database')
 
+exports.all = async() => {
+    const { rows } = await db.getPool().query("select * from authors order by id")
+    return db.camelize(rows)
+}
+
+// add check that there really is a row to return
+exports.get = async (id) => {
+    const { rows } = await db.getPool().query("select * from authors where id = $1", [id])
+    return db.camelize(rows)[0]
+}
+
+exports.create = async (author) => {
+    return await db.getPool().query("insert into authors (first_name, last_name) values ($1, $2) returning *", [author.firstName, author.lastName])
+}
+
+exports.update = async (author) => {
+    return await db.getPool().query("update authors set first_name = $1, last_name = $2 where id = $3 returning *", [author.firstName, author.lastName, author.id])
+}
+
+exports.upsert = async (author) => {
+    if (author.id) {
+        exports.update(author)
+    } else {
+        exports.add(author)
+    }
+}
+
+// old methods for authors stored in the model
+
 // const authors = [
 //     {firstName: 'Suzanne', lastName: 'Clarke'}, 
 //     {firstName: 'Robin', lastName: 'Hobb'},
@@ -9,27 +38,22 @@ const db = require('../database')
 
 // exports.all = authors
 
-exports.all = async() => {
-    const { rows } = await db.getPool().query("select * from authors order by id")
-    return db.camelize(rows)
-}
+// exports.update = (author) => {
+//     authors[author.id] = author
+// }
 
-exports.upsert = (author) => {
-    if (author.id) {
-        exports.update(author)
-    } else {
-        exports.add(author)
-    }
-}
+// exports.add = (author) => {
+//     authors.push(author)
+// }
 
-exports.update = (author) => {
-    authors[author.id] = author
-}
+// exports.get = (i) => {
+//     return authors[i]
+// }
 
-exports.add = (author) => {
-    authors.push(author)
-}
-
-exports.get = (i) => {
-    return authors[i]
-}
+// exports.upsert = (author) => {
+//     if (author.id) {
+//         exports.update(author)
+//     } else {
+//         exports.add(author)
+//     }
+// }
